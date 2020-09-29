9c8ba255ebc69cc4c5d07f99b256d64bbe767241
apeiron <alejandro.javier@apeiron.com>
2020-09-28 20:09:42 -0500

    init commit

--- Actividad1/Actividad1.py
+++ Actividad1/Actividad1.py
@@ -1,12 +1,19 @@
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
 
 GreenHouse = pd.read_csv('Database-course-material/TextFiles/greenhouse_gas_inventory_data_data.csv')
 Population = pd.read_csv('Database-course-material/TextFiles/populationbycountry19802010millions.csv')
-
-co2 = GreenHouse.iloc[:, [0,2]]
-co2Sorted = co2.sort_values(by=['value'],axis=0, ascending=False)
+co2Bool = GreenHouse['category'] == "greenhouse_gas_ghgs_emissions_including_indirect_co2_without_lulucf_in_kilotonne_co2_equivalent"
+co2 = GreenHouse[co2Bool]
+co2Value = co2.iloc[:, [0,2]]
+co2Value['value'] = co2Value['value'].astype(float)
+co2Sorted = co2Value.sort_values(by=['value'],axis=0, ascending=False)
 co2SortedWithoutDuplicates = co2Sorted.drop_duplicates(subset=['country_or_area'], keep='first')
+print("Top countries with greenhouse gas ghgs emissions")
+print("\n")
 print(co2SortedWithoutDuplicates.head(5))
+print("\n")
 
 USA = "United States"
 EU = "Europe"
@@ -21,15 +28,22 @@ C5 = Population.loc[Population['Country'] == GM]
 
 countries = C1.append(C2, ignore_index=True).append(C3, ignore_index=True).append(C4, ignore_index=True).append(C5, ignore_index=True)
 countries = countries.iloc[:, [0,31]]
+print("Top countries with greenhouse emissions Population")
+print("\n")
 print(countries)
-
+print("\n")
 topPop = Population.iloc[:, [0,31]]
-topPop = topPop.fillna("0")
-topPopSorted = topPop.sort_values(by=["2010"], axis=0, ascending=False, kind='mergesort')
-topPopSorted = topPopSorted.sort_values(by=["2010"], axis=0, ascending=False, kind='mergesort')
-#print(topPopSorted.head(50))
-max = topPopSorted.iloc[:, [1]].max()
 
-#print(max)
+topPop['2010'] = topPop['2010'].str.replace('--', '0')
+topPop['2010'] = topPop['2010'].str.replace('NA', '0')
+topPop['2010'] = topPop['2010'].astype(float)
+topPopSorted = topPop.sort_values(by=["2010"], axis=0, ascending=False, kind='mergesort')
+
+print("Top countries Population")
+print("\n")
+print(topPopSorted.head(9))
+
+
+
 
 
---
+++ Actividad1/readme-analisis.md
@@ -0,0 +1,31 @@
+¿Cuáles fueron los 5 países más productores de gases de invernadero en 2010?
+Top countries with greenhouse gas ghgs emissions
+
+               country_or_area         value
+2005  United States of America  7.422208e+06
+1348            European Union  5.664846e+06
+1847        Russian Federation  3.767555e+06
+1556                     Japan  1.412795e+06
+1423                   Germany  1.249829e+06
+
+Top countries with greenhouse emissions Population
+
+         Country       2010
+0  United States  310.23286
+1         Europe  606.00344
+2         Russia  139.39021
+3          Japan  126.80443
+4        Germany   82.28299
+
+¿Cuáles fueron los 5 países más poblados en 2010? 
+Top countries Population
+
+                     Country        2010
+193                    China  1330.14129
+200                    India  1173.10802
+127                   Africa  1015.47842
+53                    Europe   606.00344
+6              United States   310.23286
+
+¿Tienen alguna relación estos datos?
+En base a los datos no se ve que haya mucha correlacion entre las emiciones y la poblacion, solo Europe y United States se encuentran en la lista de los paises mas poblados. Russia, Japan y Germany. Por otro lado los 3 primeros paises mas poblados del mundo no son los que producen la mayor cantidad de emisiones por lo cual apartir de los datos se puede decir que hay una correlacion debil.
